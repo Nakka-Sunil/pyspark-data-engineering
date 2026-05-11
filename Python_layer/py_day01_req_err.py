8cadbad810fa951918f3f5b77022c7505c5b4371
Sunil Nakka <117005093+Nakka-Sunil@users.noreply.github.com>
2026-05-11 22:59:07 +0530

    implemented pandas dataframes

--- Python_layer/py_day01_req_err.py
+++ Python_layer/py_day01_req_err.py
@@ -4,11 +4,11 @@ import csv
 import json
 import os
 from datetime import datetime
-
+import pandas as pd
 
 date = datetime.now().strftime('%Y-%m-%d')
 
-with open('Python_layer\config.json','r') as file:
+with open('Python_layer/config.json','r') as file:
     config = json.load(file)
     # print(config)
 
@@ -26,12 +26,18 @@ url = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&h
 def fetch_weather_data():
     try:
         logger.info('API call started!!!')
-        response = requests.get(url)
+        response = requests.get(url, timeout=15)
         response.raise_for_status()
         # print(response.status_code)
         # print(response.text)
         data = response.json()
+        logger.info('Data fetched Successfully!!!')
         # print(data)
+    except requests.exceptions.Timeout:
+        logger.error('request Timed out!!!')
+        return None
+    except requests.exceptions.HTTPError:
+        logger.error('HTTP Error occured. Please check the URL once!!!')
     except Exception as e:
         logger.error(f'Something went wrong : {e}')
         return None
@@ -39,22 +45,36 @@ def fetch_weather_data():
     return data
     
 def transform_weather_data(data):
-    dataset = []
+    # dataset = []
     time = data['hourly']['time']
     temps = data['hourly']['temperature_2m']
-    for time, temp in zip(time,temps):
-        dataset.append([time,temp])
-    return dataset
+    # for t, temp in zip(time,temps):
+    #     dataset.append([t,temp])
+    # return dataset
+    dataframe = pd.DataFrame(
+        {
+            'time': time,
+            'temperature': temps
+        }
+    )
 
-def save_to_csv(data, date):
+    high_temp_df = dataframe[dataframe['temperature'] > 25]
+    #print(high_temp_df)
+
+    return high_temp_df
+
+def save_to_csv(df_data, date):
     output_dir = os.path.join('Python_layer', config['output_folder'])
     os.makedirs(output_dir, exist_ok=True)
-    full_output_path = os.path.join(output_dir, f'output_{date}.csv')
+    # full_output_path = os.path.join(output_dir, f'output_{date}.csv')
 
-    with open(full_output_path, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['time', 'temperature_2m'])
-        writer.writerows(data)
+    # with open(full_output_path, 'w', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(['time', 'temperature_2m'])
+    #     writer.writerows(data)
+    #print(df_data)
+    df = pd.DataFrame(df_data, columns=['time', 'temperature'])
+    df.to_csv(f'{output_dir}/pandas_df_output_{date}.csv',index = False)
     logger.info('File saved successfully!')
             
 if __name__ == '__main__':
